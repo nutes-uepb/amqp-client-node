12ecea8d2af3eabedd017fc9e1fdf9ec525d1cfd
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2019-08-11 14:56:37 -0300

    Added some integration tests.

---
+++ test/integration/amqp.client.node.spec.ts
@@ -0,0 +1,174 @@
+import { expect } from 'chai'
+import { amqpClient } from '../../src/amqp.client'
+import { Connection } from '../../src/application/connection'
+import { IConnection } from '../../src/application/port/connection.interface'
+
+describe('AMQP CLIENT NODE', () => {
+    describe('CONNECTION', () => {
+        it('should return an error when unable to connect.', () => {
+            return amqpClient
+                .createConnetion('amqp://test:test@host-test',
+                    { retries: 1, interval: 100 })
+                .catch(e => {
+                    expect(e).to.be.an('error')
+                })
+        })
+
+        it('should return instance of connection when connecting.', () => {
+            return amqpClient
+                .createConnetion('amqp://guest:guest@localhost',
+                    { retries: 1, interval: 1000 })
+                .then(conn => {
+                    expect(conn).to.be.an.instanceof(Connection)
+                })
+        })
+    })
+
+    describe('PUBLISH', () => {
+        context('Publish Unsuccessfully', () => {
+            let conn: IConnection
+            before(async () => {
+                conn = await getConnection()
+            })
+
+            after(async () => {
+                await conn.dispose()
+            })
+            it('should return error when trying to publish without connection', async () => {
+                const noConn = await getConnection()
+                await noConn.close()
+
+                return noConn
+                    .pub(
+                        'test.queue',
+                        'test.exchange',
+                        'log.info'
+                    )
+                    .then(() => {
+                        expect.fail('should not return promise with no error!')
+                    })
+                    .catch(err => {
+                        expect(err).to.be.an('error')
+                    })
+            })
+
+            // it('should return error when trying to publish with different exchange options already created', async () => {
+            //     try {
+            //         await conn
+            //             .pub(
+            //                 'test.queue1',
+            //                 'test.exchange1',
+            //                 'log.info',
+            //                 {
+            //                     exchange: {
+            //                         type: 'direct',
+            //                         durable: false
+            //                     }
+            //                 }
+            //             )
+            //
+            //         await conn
+            //             .pub(
+            //                 'test.queue1',
+            //                 'test.exchange1',
+            //                 'log.info',
+            //                 {
+            //                     exchange: {
+            //                         type: 'topic',
+            //                         durable: true
+            //                     }
+            //                 }
+            //             )
+            //     } catch (e) {
+            //         expect(e).to.be.an('error')
+            //     }
+            // })
+        })
+
+        context('Publish Successfully', async () => {
+            let conn: IConnection
+            before(async () => {
+                conn = await getConnection()
+            })
+
+            after(async () => {
+                await conn.dispose()
+            })
+            it('should return a successful promise to publish using default options', async () => {
+                return conn
+                    .pub(
+                        'test.queue2',
+                        'test.exchange2',
+                        'log.info'
+                    )
+                    .catch(e => {
+                        expect.fail('should not return error!', e)
+                    })
+            })
+        })
+    })
+
+    describe('SUBSCRIBE', () => {
+        context('Unsuccessfully', () => {
+            it('should return error when trying to subscribe without connection', async () => {
+                const conn = await getConnection()
+                await conn.close()
+
+                return conn
+                    .sub(
+                        'test.queue',
+                        'test.exchange',
+                        'log.info',
+                        () => {
+                            // not implemented
+                        }
+                    )
+                    .then(() => {
+                        expect.fail('should not return promise with no error!')
+                    })
+                    .catch(e => {
+                        expect(e).to.be.an('error')
+                    })
+            })
+        })
+
+        context('Successfully', async () => {
+            let conn: IConnection
+            before(async () => {
+                conn = await getConnection()
+            })
+
+            after(async () => {
+                await conn.dispose()
+            })
+
+            it('should return Promise void to successfully subscribe', async () => {
+                return conn
+                    .sub(
+                        'test.queue',
+                        'test.exchange',
+                        'log.info',
+                        () => {
+                            // not implemented
+                        }
+                    )
+                    .catch(e => {
+                        expect.fail('should not return error!')
+                    })
+            })
+        })
+    })
+
+    describe('LOGGER', () => {
+        it('should have successfully enabled logger', () => {
+            amqpClient.logger('info')
+        })
+    })
+})
+
+async function getConnection(): Promise<IConnection> {
+    return amqpClient.createConnetion(
+        'amqp://guest:guest@localhost',
+        { retries: 1, interval: 1000 }
+    )
+}
